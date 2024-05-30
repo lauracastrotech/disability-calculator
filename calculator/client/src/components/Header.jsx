a4717674115e768730287e4bd734464b68855c84
Laura Castro <lauracastrotech@gmail.com>
2024-05-30 14:25:38 -0400

    refactored code to test api

--- calculator/client/src/components/Header.jsx
+++ calculator/client/src/components/Header.jsx
@@ -1,25 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
+// import DeepLTranslator from "./DeepL";
 
 export default function Header() {
-    const [translatedText, setTranslatedText] = useState('');
-
-    useEffect(() => {
-        const fetchTranslation = async () => {
-            try {
-                const response = await fetch('/translate');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setTranslatedText(data.text);
-            } catch (error) {
-                console.error("Error fetching translation:", error);
-            }
-        };
-        
-        fetchTranslation();
-    }, []);
     
     return (
         <div className="container">
@@ -32,24 +15,9 @@ export default function Header() {
                 This calculator takes your monthly income and the date that you began working. It will give you the
                 month that you will stop receiving benefits.
             </p>
-            <p>{translatedText}</p>
+            {/* <DeepLTranslator /> */}
         </div>
     );
 }
 
-
-// export default function Header() {
-
-//     return (
-//         <>
-//             <div className="container">
-//                 <h4>
-//                 <span className="material-symbols-outlined m-2"> confirmation_number</span>
-//                 Ticket to Work Period Calculator
-//                 </h4>
-//                 <p className="m-2">If you are enrolled in Ticket to Work, you know that you continue to receive disability benefits. This calculator takes your monthly income and the date that you began working. It will give you the month that you will stop receiving benefits.</p>
-//             </div>
-//         </>
-//     )
-// }
 
